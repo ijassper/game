bef4e4802cdfad5d6e401f38cf72c5ca181101b0
ijassper <jasspercoach@gmail.com>
2026-03-18 21:23:34 +0900

    Add basic stop game functionality with Streamlit

---
+++ main.py
@@ -0,0 +1,11 @@
+import random as rd
+import streamlit as st
+
+st.tilte("Stop Game")
+
+if st.button("Game Start"):
+  num = rd.random()
+    if num < 0.1:
+      st.error("STOP")
+    else:
+      st.success("PASS")
